f1061b158b8df6ac21b56c0a968b7b51f46136bb
Ty Craig <tylerlcraig@proton.me>
2024-02-20 21:49:23 -0700

    completed snowflakes exercise

--- snowflakes/snowflakes.c
+++ snowflakes/snowflakes.c
@@ -1,22 +1,113 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void identify_identical(int values[], int n) {
-    int i, j;
+#define SIZE 100000
 
-    for(i = 0; i < n; i++) {
-        for (j = i + 1; j < n; j ++) {
-            if(values[i] == values[j]) {
-                printf("Twin integers found.\n");
-                return;
-            }
+typedef struct snowflake_node {
+    int snowflake[6];
+    struct snowflake_node *next;
+
+} snowflake_node;
+
+
+int hash(int snowflake[]) {
+    return (snowflake[0] + snowflake[1] + snowflake[2] + snowflake[3] + snowflake[4] + snowflake[5]) % SIZE;
+}
+
+int identical_right(int snow1[], int snow2[], int start) {
+    int offset;
+ 
+    for(offset = 0; offset < 6; offset++) {
+        if(snow1[offset] != snow2[(start + offset) % 6]) {
+            return 0;
         }
     }
+
+    return 1;
+}
+
+int identical_left(int snow1[], int snow2[], int start) {
+    int offset, snow2_index;
+
+    for(offset = 0; offset < 6; offset++) {
+        snow2_index = start - offset;
+
+        if(snow2_index < 0) {
+            snow2_index = snow2_index + 6;
+        }
+
+        if(snow1[offset] != snow2[snow2_index]) {
+            return 0;
+        }
+    }
+
+    return 1;
+}
+
+int are_identical(int snow1[], int snow2[]) {
+    int start;
+
+    for(start = 0; start < 6; start++) {
+        if(identical_right(snow1, snow2, start)) {
+            return 1;
+        }
+        if(identical_left(snow1, snow2, start)) {
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
+void identify_identical(snowflake_node *snowflakes[]) {
+    int i;
+    snowflake_node *node1, *node2;
+
+    for(i = 0; i < SIZE; i++) {
+        node1 = snowflakes[i];
+
+        while(node1 != NULL) {
+            node2 = node1->next;
+            while(node2 != NULL) {
+                if(are_identical(node1->snowflake, node2->snowflake)) {
+                    printf("Twin snowflakes found.\n");
+                    return;
+                }
+                node2 = node2->next;
+            }
+            node1 = node1->next;
+        }
+    }
+
+    printf("No two snowflakes are alike.\n");
 }
 
 int main(void) { 
-    int a[5] = {1, 2, 3, 4, 1};
+    static snowflake_node *snowflakes[SIZE] = {NULL};
+    snowflake_node *snow;
 
-    identify_identical(a, 5);
+    int n, i, j, snowflake_hash;
+
+    scanf("%d", &n);
+
+    for(i = 0; i < n; i++) {
+        snow = malloc(sizeof(snowflake_node)); // Allocate memory for snowflake_node 
+        if(snow == NULL) {
+            fprintf(stderr, "malloc error\n");
+            exit(1);
+        }
+
+        for(j = 0; j < 6; j++) {
+            scanf("%d", &snow->snowflake[j]);
+        }
+
+        snowflake_hash = hash(snow->snowflake);
+        snow->next = snowflakes[snowflake_hash];
+        snowflakes[snowflake_hash] = snow;
+    }
+
+    identify_identical(snowflakes);
+
     return 0;
 }
 
